348a91d792b578657ba7805682e91717bc6758a0
Sean Del Castillo <seanbdcastillo@gmail.com>
2023-09-22 11:07:27 -0700

    Working on VisionAwareness system

--- VisionAwareness.cs
+++ VisionAwareness.cs
@@ -1,6 +1,7 @@
 using Godot;
 using Godot.Collections;
 using System;
+using System.Collections.Generic;
 
 public partial class VisionAwareness : Node3D
 {
@@ -8,28 +9,57 @@ public partial class VisionAwareness : Node3D
 	public Area3D AwarenessArea { set; get; }
 	[Export]
 	public RayCast3D VisionCast { set; get; }
+	[Export]
+	public CharacterBody3D Self { set; get; }
 
-	private System.Collections.ArrayList _bodiesInRange;
+	private List<Node3D> _bodiesInAwarenessRange = new();
+	private List<Node3D> _bodiesInVisualContact = new();
 
 	public override void _Ready()
 	{
 		AwarenessArea.BodyEntered += OnAwarenessAreaBodyEntered;
+		AwarenessArea.BodyExited += OnAwarenessAreaBodyExited;
 	}
 
 	public override void _Process(double delta)
 	{
-		if (_bodiesInRange.Count > 0)
+		for (int i = 0; i < _bodiesInAwarenessRange.Count; i++)
 		{
-			for (int i = 0; i < _bodiesInRange.Count; i++)
+			VisionCast.TargetPosition = _bodiesInAwarenessRange[i].GlobalPosition * GlobalTransform;
+			if (VisionCast.GetCollider() as Node3D == _bodiesInAwarenessRange[i])
 			{
-				var body = _bodiesInRange[i] as Node3D;
-				VisionCast.TargetPosition = Transform * body.GlobalPosition;
+				_bodiesInVisualContact.Add(_bodiesInAwarenessRange[i]);
 			}
+		}
+
+		for (int i = 0; i < _bodiesInVisualContact.Count; i++)
+		{
+			VisionCast.TargetPosition = _bodiesInVisualContact[i].GlobalPosition * GlobalTransform;
+			if (VisionCast.GetCollider() as Node3D != _bodiesInVisualContact[i])
+			{
+				_bodiesInVisualContact.Remove(_bodiesInVisualContact[i]);
+			}
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_bodiesInVisualContact.Count > 0)
+		{
+			GetParent<Node3D>().LookAt(_bodiesInVisualContact[0].GlobalPosition);
 		}
 	}
 
 	private void OnAwarenessAreaBodyEntered(Node3D body)
 	{
-		_bodiesInRange.Add(body);
+		if (body is CharacterBody3D && body != Self)
+		{
+			_bodiesInAwarenessRange.Add(body);
+		}
+	}
+
+	private void OnAwarenessAreaBodyExited(Node3D body)
+	{
+		_bodiesInAwarenessRange.Remove(body);
 	}
 }
